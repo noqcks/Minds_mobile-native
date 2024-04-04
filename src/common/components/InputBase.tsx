cd7db7d3d9471f5947469376dbff56d17e68fe5a
noqcks <benji@093b.org>
2024-04-04 14:59:50 +0000

    Upgrade dependencies

--- src/common/components/InputBase.tsx
+++ src/common/components/InputBase.tsx
@@ -81,8 +81,9 @@ export default function InputBase({
             width={200}
             height={100}
             backgroundColor={ThemedStyles.getColor('Link')}
-            popover={<MText style={theme.colorWhite}>{info}</MText>}>
-            <Icon left="S" name="info-outline" bottom="S" />
+            popover={<MText style={theme.colorWhite}>{info}</MText>}
+          >
+            {<Icon left="S" name="info-outline" bottom="S" />}
           </Tooltip>
         )}
         {icon ? icon : null}
